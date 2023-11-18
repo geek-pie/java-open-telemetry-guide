4822c3bce5b06d8ea8cfd00e95f8178357e27ec5
xienng <xienng@qq.com>
2023-11-18 10:18:16 +0800

    Add files via upload

---
+++ open-telemetry/src/main/java/com/bscoder/controller/Application.java
@@ -0,0 +1,17 @@
+package com.bscoder.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author xujianxing@sensetime.com
+ * @date 2023年09月04日 15:34
+ */
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
